e4b691a7f2fc189e531e7c4ce0a492920916676a
Ziru Zou <jiyucoding@yahoo.com>
2023-02-03 00:00:07 -0500

    final commit

--- src/components/Minutes.js
+++ src/components/Minutes.js
@@ -7,20 +7,20 @@ function Minutes({minutes}) {
 
     function renderEmojis() {
         let quotient
-        let remainer
+        let remainder
 
         if (minutes <= 30 ) {
             quotient = parseInt(parseInt(minutes) / 5)
-            remainer = minutes % 5    
-            if (remainer > 0) {
+            remainder = minutes % 5    
+            if (remainder > 0) {
                 return coffeeCupEmoji.repeat(quotient+1)
             } else {
                 return coffeeCupEmoji.repeat(quotient)
             }
         } else {
             quotient = parseInt(parseInt(minutes) / 10)
-            remainer = minutes % 10    
-            if (remainer > 0) {
+            remainder = minutes % 10    
+            if (remainder > 0) {
                 return bentoBoxEmoji.repeat(quotient+1)
             } else {
                 return bentoBoxEmoji.repeat(quotient)
