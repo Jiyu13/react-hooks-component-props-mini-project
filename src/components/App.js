6f9ad77b9e44b140b05eab0ee6e841c9f528f34a
Ziru Zou <jiyucoding@yahoo.com>
2023-02-02 21:34:14 -0500

    Make an ArticleList component as child of App, pass down blogData.posts as prop

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header"
 import About from "./About"
+import ArticleList from "./ArticleList"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header blogData={blogData}/>
       <About about={blogData.about}/>
+      <ArticleList posts={blogData.posts}/>
     </div>
   );
 }
