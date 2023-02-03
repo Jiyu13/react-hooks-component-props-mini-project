e7809ed91a2578502ca4dfbea90513233a0b4470
Ziru Zou <jiyucoding@yahoo.com>
2023-02-02 21:13:06 -0500

    Make an ABout component as a child of App, pass in blogData.about as prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header"
+import About from "./About"
 
 
 function App() {
   return (
     <div className="App">
       <Header blogData={blogData}/>
+      <About about={blogData.about}/>
     </div>
   );
 }
